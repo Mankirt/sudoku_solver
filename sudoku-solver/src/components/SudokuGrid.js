2bff2795868658bab33f931c4309f9f23b9e0307
Mankirat <mankirat2601@gmail.com>
2025-12-23 22:22:36 -0800

    Setting input max length to 1

--- sudoku-solver/src/components/SudokuGrid.js
+++ sudoku-solver/src/components/SudokuGrid.js
@@ -283,6 +283,7 @@ function SudokuGrid() {
                 Array.from({ length: 9 }).map((_, colIdx) => (
                   <td key={colIdx} className={(colIdx + 1) % 3 === 0 ? 'gridColBorder' : ""}>
                     <input
+                      maxLength={1}
                       onChange={(e) => onGridChange(e, rowIdx, colIdx)}
                       value={sudokuGrid[rowIdx][colIdx] === -1 ? '' : sudokuGrid[rowIdx][colIdx]}
                       className="cellInput"
